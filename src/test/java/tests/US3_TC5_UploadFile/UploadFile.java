64b6ade5d01669042cb2a761577aa27ae0bc7953
Polina <polinavasylega@yahoo.com>
2021-02-07 15:14:52 -0600

    UploadFile was updated

--- src/test/java/tests/US3_TC5_UploadFile/UploadFile.java
+++ src/test/java/tests/US3_TC5_UploadFile/UploadFile.java
@@ -3,43 +3,76 @@ package tests.US3_TC5_UploadFile;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebElement;
 import org.testng.Assert;
+import org.testng.annotations.AfterClass;
+import org.testng.annotations.BeforeClass;
 import org.testng.annotations.Test;
 import pageObjects.LogInPage;
 import utilities.ConfigurationReader;
 import utilities.WebDriverFactory;
 
 public class UploadFile {
+
+    @BeforeClass
+    public void setUPDriver() {
+        WebDriverFactory.setUpDriver();
+        // WebDriverFactory.navigateToURL(ConfigurationReader.getProperties("environment"));
+        WebDriverFactory.navigateToURL("http://qa3.trycloud.net");
+    }
+
+
     @Test(description = "Verify users can upload a file directly to the homepage")
     public void uploadFile() {
-        // 1. Login as a User
-        WebDriverFactory.setUpDriver();
-        WebDriverFactory.navigateToURL(ConfigurationReader.getProperties("environment"));
-        //WebDriverFactory.navigateToURL("http://app.trycloud.net/index.php/login");
 
+        // 1. Login as a User
         LogInPage logInPage = new LogInPage();
         logInPage.UserLogIn.sendKeys("User26");
         logInPage.UserPassword.sendKeys("Userpass123");
         logInPage.LoginBtn.click();
 
-        String path = "C:\\Users\\Polina\\Desktop\\Java Programming B21\\Study Materials\\String pool.png";
+        WebElement files = WebDriverFactory.getDriver().findElement(By.xpath("//a[@href='/index.php/apps/files/']"));
+        // @FindBy(how = How.XPATH, using = "//a[@href='/index.php/apps/files/']")
+        //    public WebElement filesLink;
+        files.click();
 
-        WebElement plusButton = WebDriverFactory.getDriver().findElement(By.cssSelector("a[class='button new']"));
+        WebDriverFactory.implicitWait();
+        // 2. Click the "+" icon on top
+        WebElement plusButton = WebDriverFactory.getDriver().findElement(By.xpath("//a[@class='button new']"));
         // @FindBy(how = How.XPATH, using = "a[class='button new']")
         //    public WebElement plusButton;
-        WebElement uploadFile = WebDriverFactory.getDriver().findElement(By.xpath("//span[.='Upload file']"));
+        // plusButton.click();
+
+        // 3. Click "upload file"
+        String path = "C:\\Users\\Polina\\Desktop\\text document.txt";
+
+        WebElement uploadFile = WebDriverFactory.getDriver().findElement(By.xpath("//input[@type='file']"));
         // @FindBy(how = How.XPATH, using = "//span[.='Upload file']")
         //    public WebElement uploadFile;
 
-        // 2. Click the "+" icon on top
-        // 3. Click "upload file"
         // 4. Upload a file
-        plusButton.click();
-        uploadFile.click();
         uploadFile.sendKeys(path);
 
         // 5. Verify the file is displayed on the page
-        WebElement fileUploaded = WebDriverFactory.getDriver().findElement(By.xpath("//span[@class='nametext']"));
-        Assert.assertTrue(fileUploaded.isDisplayed(), "File is not displayed on the page");
+        // WebElement fileUploaded = WebDriverFactory.getDriver().findElement(By.xpath("//span[.='String pool']"));
+        //  Assert.assertTrue(fileUploaded.isDisplayed(), "File is displayed on the page");
 
+        /*WebDriverFactory.implicitWait();
+
+        if (WebDriverFactory.getDriver().findElement(By.xpath("//span[.='String pool']")).isDisplayed()) {
+            Assert.assertTrue(true, "File is displayed on the page!!!");
+            System.out.println("yes");
+        } else {
+            Assert.assertTrue(false, "File is NOT displayed on the page!");
+            System.out.println("no");
+        }
+*/
     }
+
+/*    @AfterClass
+    public void tearDown() {
+        WebDriverFactory.killDriver();
+        WebDriverFactory.implicitWait();
+    }*/
+
+
 }
+
