6b92ad99dad063c431336ad105980e57bcf47fe3
Aidai Chokoeva <chokoeva01@gmail.com>
2021-02-12 22:42:54 -0500

    browserutils added

--- src/test/java/tests/US2_TC1_MainModules/AccessMainModules.java
+++ src/test/java/tests/US2_TC1_MainModules/AccessMainModules.java
@@ -1,22 +1,50 @@
 package tests.US2_TC1_MainModules;
 
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebElement;
+import org.testng.Assert;
 import org.testng.annotations.BeforeClass;
 import org.testng.annotations.Test;
 import pageObjects.LogInPage;
+import utilities.BrowserUtils;
 import utilities.ConfigurationReader;
 import utilities.WebDriverFactory;
 import java.util.Properties;
 
-public class AccessMainModules extends LogInPage{
+public class AccessMainModules extends LogInPage {
     // WebDriverFactory.navigateToURL(ConfigurationReader.getProperties("environment3"));
     @BeforeClass
     public void setUpDriver() {
         WebDriverFactory.setUpDriver();
         WebDriverFactory.navigateToURL("http://qa3.trycloud.net");
 
-}
-@Test
-    public void TC1(){
+    }
+
+    @Test
+    public void TC1() {
+        // 1. Login as a User
+        LogInPage logInPage = new LogInPage();
+        logInPage.UserLogIn.sendKeys("User26");
+        logInPage.UserPassword.sendKeys("Userpass123");
+        logInPage.LoginBtn.click();
+    }
+
+    @Test
+    public void fileModule() {
+        //Verify the user see the modules File:
+       WebElement filesLink = WebDriverFactory.getDriver().findElement(By.xpath("//a[@href='/index.php/apps/files/']"));
+       Assert.assertTrue(filesLink.isDisplayed());
+       BrowserUtils.sleep(1);
+
+
+       //BrowserUtils.hoover(filesLink);
+       // String expectedFilePageTitle = "Files";
+
+     //  Assert.assertTrue(WebDriverFactory.getDriver().getTitle().contains(expectedFilePageTitle));
+
+
+
+
 
 
     }
--- src/test/java/utilities/BrowserUtils.java
+++ src/test/java/utilities/BrowserUtils.java
@@ -1,8 +1,24 @@
 package utilities;
 
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.interactions.Actions;
+
 public class BrowserUtils {
+    public static void hoover(WebElement element) {
+        Actions actions = new Actions(WebDriverFactory.getDriver());
+        actions.moveToElement(element).perform();
+    }
 
 
+        public static void sleep(int seconds){
 
+            try {
+                Thread.sleep(seconds*1000);
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+        }
 
-}
+    }
+
