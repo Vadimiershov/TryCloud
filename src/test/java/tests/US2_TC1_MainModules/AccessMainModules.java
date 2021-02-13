e80e97e1301a3c22db2dd42466bd5dee9c2ffa04
Aidai Chokoeva <chokoeva01@gmail.com>
2021-02-12 23:44:04 -0500

    Done with US2 TC1. Verify user see the following modules

--- src/test/java/tests/US2_TC1_MainModules/AccessMainModules.java
+++ src/test/java/tests/US2_TC1_MainModules/AccessMainModules.java
@@ -3,8 +3,10 @@ package tests.US2_TC1_MainModules;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebElement;
 import org.testng.Assert;
+import org.testng.annotations.AfterMethod;
 import org.testng.annotations.BeforeClass;
 import org.testng.annotations.Test;
+import pageObjects.HomePageHeader;
 import pageObjects.LogInPage;
 import utilities.BrowserUtils;
 import utilities.ConfigurationReader;
@@ -31,24 +33,62 @@ public class AccessMainModules extends LogInPage {
 
     @Test
     public void fileModule() {
-        //Verify the user see the modules File:
+        //1. Verify the user see the modules File:
        WebElement filesLink = WebDriverFactory.getDriver().findElement(By.xpath("//a[@href='/index.php/apps/files/']"));
        Assert.assertTrue(filesLink.isDisplayed());
-    //   BrowserUtils.sleep(1);
+      //  WebDriverFactory.implicitWait();
+       // filesLink.click();
 
-
-       //BrowserUtils.hoover(filesLink);
-       // String expectedFilePageTitle = "Files";
-
-     //  Assert.assertTrue(WebDriverFactory.getDriver().getTitle().contains(expectedFilePageTitle));
-
-
-
-
-
+      //  //2. Get text from module Expect Result "File"
+     //   String expectedFilePageTitle = "Files";
+    //    Assert.assertTrue(WebDriverFactory.getDriver().getTitle().contains(expectedFilePageTitle));
+    }
+        @Test
+        //1. Verify the user see the gallery module:
+        public void galleryModule(){
+            WebElement photosLink = WebDriverFactory.getDriver().findElement(By.xpath("(//a[@aria-label='Photos'])[1]"));
+            Assert.assertTrue(photosLink.isDisplayed());
+          //  WebDriverFactory.implicitWait();
+    }
+    @Test
+    //1. Verify the user see the activityModule:
+    public void activityModule(){
+        WebElement activityLink = WebDriverFactory.getDriver().findElement(By.xpath("(//a[@aria-label='Activity'])[1]"));
+        Assert.assertTrue(activityLink.isDisplayed());
+      //  WebDriverFactory.implicitWait();
 
     }
-}
+
+    @Test
+    //1. Verify the user see the talk  modules :
+    public void talkModule(){
+        WebElement talkLink = WebDriverFactory.getDriver().findElement(By.xpath("(//a[@aria-label='Talk'])[1]"));
+        Assert.assertTrue(talkLink.isDisplayed());
+      //  WebDriverFactory.implicitWait();
+    }
+    @Test
+    //1. Verify the user see the contacts modules :
+    public void contactsModule(){
+        WebElement contactLink = WebDriverFactory.getDriver().findElement(By.xpath("(//a[@aria-label='Contacts'])[1]"));
+        Assert.assertTrue(contactLink.isDisplayed());
+      //  WebDriverFactory.implicitWait();
+    }
+    @Test
+    //1. Verify the user see the calender modules :
+    public void calenderModule(){
+        WebElement calendarLink = WebDriverFactory.getDriver().findElement(By.xpath("(//a[@aria-label='Calendar'])[1]"));
+        Assert.assertTrue(calendarLink.isDisplayed());
+       // WebDriverFactory.implicitWait();
+
+    }
+  //  @Test
+//    public void noteModule(){
+     //   WebElement notesLink = WebDriverFactory.getDriver().findElement(By.xpath("(//a[@aria-label='Notes'])[1]"));
+    //    Assert.assertTrue(notesLink.isDisplayed());
+    //    WebDriverFactory.implicitWait();
+    }
+
+
 
 
 
